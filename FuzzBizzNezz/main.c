c4ec55f0ff4e023e79d71b40641c8a0e5d00cd27
Alp Eren Can <alperenc@gmail.com>
2015-06-05 17:25:59 +0300

    Added user input and completed implementation.

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -10,18 +10,34 @@
 
 int main(int argc, const char * argv[]) {
     
+    int fuzz;
+    int bizz;
+    int nezz;
+    
+    printf("Type in a number to replace with the word Fuzz: ");
+    scanf("%i", &fuzz);
+    printf("You entered: %i\n", fuzz);
+    
+    printf("Type in another number to replace with the word Bizz: ");
+    scanf("%i", &bizz);
+    
+    printf("Type in a number to count to: ");
+    scanf("%i", &nezz);
+    
     int counter = 1;
-    while (counter <= 100) {
-        if (counter % 3 == 0 && counter % 5 == 0) {
-            printf("FizzBuzz\n");
-        } else if (counter % 3 == 0) {
-            printf("Fizz\n");
-        } else if (counter % 5 == 0) {
-            printf("Buzz\n");
+    while (counter <= nezz) {
+        if (counter % fuzz == 0 && counter % bizz == 0) {
+            printf("FuzzBizz\n");
+        } else if (counter % fuzz == 0) {
+            printf("Fuzz\n");
+        } else if (counter % bizz == 0) {
+            printf("Bizz\n");
         } else {
             printf("%i\n", counter);
         }
         counter++;
     }
+    
+    printf("The counting complete. Check the results.");
     return 0;
 }
